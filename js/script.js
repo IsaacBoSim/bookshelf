091acb59f95091e7799c9726c3b91db01b1394c6
IsaacBoSim <hyz5yn@virginia.edu>
2024-06-10 13:03:05 -0400

    deleted branches

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
+// Function to load content from external HTML files into specified elements
+function loadContent(url, elementId) {
+    fetch(url)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById(elementId).innerHTML = data;
+        })
+        .catch(error => console.error('Error loading content:', error));
+}
+
+// Load content from about.html into the element with id 'about-content'
+loadContent('about.html', 'about-content');
+
+// Load content from project.html into the element with id 'project-content'
+loadContent('project.html', 'project-content');
+
+// Load content from blog.html into the element with id 'blog-content'
+loadContent('blog.html', 'blog-content');
+
+// Load content from contact.html into the element with id 'contact-content'
+loadContent('contact.html', 'contact-content');
+
 document.querySelectorAll('.page').forEach((page, index, pages) => {
     page.addEventListener('click', function() {
         // Toggle the flipped class to manage the flip state
@@ -7,7 +29,6 @@ document.querySelectorAll('.page').forEach((page, index, pages) => {
             this.style.transform = 'rotateY(-180deg)';
             this.querySelector('.back').style.transform = 'rotateY(0deg)';
             this.querySelector('.front').style.opacity = 0;
-
         } else {
             // When flipped back, rotate to show the front
             this.style.transform = 'rotateY(0deg)';

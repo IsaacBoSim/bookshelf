e4e1e3ab0eded82a90c82c733d26768ae96f8434
IsaacBoSim <hyz5yn@virginia.edu>
2024-06-10 21:59:29 -0400

    added bookmark navbar feature

--- js/script.js
+++ js/script.js
@@ -24,3 +24,35 @@ document.querySelectorAll('.page').forEach((page, index, pages) => {
         });
     });
 });
+
+        function navigateToPage(pageIndex) {
+            const pages = document.querySelectorAll('.page');
+            pages.forEach((page, index) => {
+                if (index < pageIndex) {
+                    setTimeout(() => {
+                        page.classList.add('flipped');
+                        page.style.transform = 'rotateY(-180deg)';
+                        page.querySelector('.back').style.transform = 'rotateY(0deg)';
+                        page.querySelector('.front').style.opacity = 0;
+                        page.style.zIndex = -(pages.length - index);
+                    }, 100 * index); // Add delay based on index
+                } else if (index === pageIndex) {
+                    setTimeout(() => {
+                        page.classList.remove('flipped');
+                        page.style.transform = 'rotateY(0deg)';
+                        page.querySelector('.back').style.transform = 'rotateY(180deg)';
+                        page.querySelector('.front').style.opacity = 1;
+                        page.style.zIndex = pages.length - index;
+                    }, 100 * index); // Add delay based on index
+                } else {
+                    setTimeout(() => {
+
+                        page.classList.remove('flipped');
+                        page.style.transform = 'rotateY(0deg)';
+                        page.querySelector('.back').style.transform = 'rotateY(180deg)';
+                        page.querySelector('.front').style.opacity = 1;
+                        page.style.zIndex = pages.length - index;
+                        }, 100 * index); // Add delay based on index
+                }
+            });
+        }
